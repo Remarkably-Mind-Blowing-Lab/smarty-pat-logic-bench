0639b5d4dac45b3e649d83d4a2b0f794e946b060
Yuekang Li <yli044@e.ntu.edu.sg>
2025-02-27 17:11:40 +1100

    updated gh page config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,20 +10,20 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Smarty-Pat: An LLM Logic Reason Benchmark',
+  tagline: 'Smarty Pat',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://remarkably-mind-blowing-lab.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/smarty-pat-logic-bench',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Remarkably-Mind-Blowing-Lab', // Usually your GitHub org/user name.
+  projectName: 'smarty-pat-logic-bench', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
