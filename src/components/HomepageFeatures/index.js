2cdacfc5408022100c970b2b6046bb655b551195
Yuekang Li <yli044@e.ntu.edu.sg>
2025-02-27 21:21:28 +1100

    sample leader board

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,93 +1,120 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
+import React, { useRef, useState } from 'react';
 import styles from './styles.module.css';
 
-import {Card, Col, Row, Divider, Timeline, Space, Table, Tag} from 'antd';
+import {Card, Col, Row, Divider, Timeline, Space, Table, Tag, Form, Radio, Switch, List, Button, Input } from 'antd';
 import { PlusCircleOutlined, MinusCircleOutlined } from '@ant-design/icons';
 
 const {Meta} = Card;
 
 // import useBaseUrl from '@docusaurus/useBaseUrl';
 
-const columns = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        render: (text) => <a>{text}</a>,
-    },
-    {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age',
-    },
-    {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
-    },
-    {
-        title: 'Tags',
-        key: 'tags',
-        dataIndex: 'tags',
-        render: (_, { tags }) => (
-            <>
-                {tags.map((tag) => {
-                    let color = tag.length > 5 ? 'geekblue' : 'green';
-                    if (tag === 'loser') {
-                        color = 'volcano';
-                    }
-                    return (
-                        <Tag color={color} key={tag}>
-                            {tag.toUpperCase()}
-                        </Tag>
-                    );
-                })}
-            </>
-        ),
-    },
-    {
-        title: 'Action',
-        key: 'action',
-        render: (_, record) => (
-            <Space size="middle">
-                <a>Invite {record.name}</a>
-                <a>Delete</a>
-            </Space>
-        ),
-    },
-];
-const data = [
-    {
-        key: '1',
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
-        tags: ['nice', 'developer'],
-    },
-    {
-        key: '2',
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park',
-        tags: ['loser'],
-    },
-    {
-        key: '3',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sydney No. 1 Lake Park',
-        tags: ['cool', 'teacher'],
-    },
-];
+import resultList from '@site/static/data/sample.json';
+
+const uniqueModels = [...new Set(resultList.map(record => record.model))];
+const modelFilters = uniqueModels.map((model) => ({text: model, value: model}));
+
+const uniqueOrgs = [...new Set(resultList.map(record => record.org))];
+const orgFilters = uniqueOrgs.map((org) => ({text: org, value: org}));
+
+const defaultTitle = () => 'Leaderboard';
 
 export default function HomepageFeatures() {
+    const [bordered, setBordered] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [size, setSize] = useState('middle');
+    const [showTitle, setShowTitle] = useState(false);
+    const [tableLayout, setTableLayout] = useState();
+    const [ellipsis, setEllipsis] = useState(false);
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef(null);
+
+    const columns = [
+        {
+            title: 'Model',
+            dataIndex: 'model',
+            key: 'model',
+            // filters: modelFilters,
+            // onFilter: (value, record) => record.venue === value,
+        },
+        {
+            title: 'Org',
+            dataIndex: 'org',
+            key: 'org',
+            filters: orgFilters,
+            onFilter: (value, record) => record.org === value,
+        },
+        {
+            title: 'F1 Score',
+            dataIndex: 'f1',
+            key: 'f1',
+            sorter: (a, b) => a.f1 - b.f1,
+            sortDirections: ['descend', 'ascend'],
+        },
+        {
+            title: 'False Positive',
+            dataIndex: 'false_positive',
+            key: 'false_positive',
+            sorter: (a, b) => a.false_positive - b.false_positive,
+            sortDirections: ['descend', 'ascend'],
+        },
+        {
+            title: 'False Negative',
+            dataIndex: 'false_negative',
+            key: 'false_negative',
+            sorter: (a, b) => a.false_negative - b.false_negative,
+            sortDirections: ['descend', 'ascend'],
+        },
+        {
+            title: 'Fallacy Labelling Score',
+            dataIndex: 'fallacy_label_score',
+            key: 'fallacy_label_score',
+            sorter: (a, b) => a.fallacy_label_score - b.fallacy_label_score,
+            sortDirections: ['descend', 'ascend'],
+        },
+        {
+            title: 'Reasoning Score',
+            dataIndex: 'reasoning_score',
+            key: 'reasoning_score',
+            sorter: (a, b) => a.reasoning_score - b.reasoning_score,
+            sortDirections: ['descend', 'ascend'],
+        },
+        {
+            title: 'Date',
+            dataIndex: 'date',
+            key: 'date',
+            sorter: (a, b) => a.date - b.date,
+            sortDirections: ['descend', 'ascend'],
+        },
+    ];
+
+
+    const tableColumns = columns.map((item) => ({
+        ...item,
+        ellipsis,
+    }));
+
     return (
         <section className={styles.features}>
             <div className="container">
-                <Row>
-                    <Table columns={columns} dataSource={data} />
-                </Row>
+                <Divider orientation="left">Introduction</Divider>
+                <div style={{
+                    // background: "#EFF2F5",
+                    padding: "16px",
+                    textAlign: "center",
+                    marginLeft: "10%",
+                    marginRight: "10%"
+                }}>
+                    <Table
+                        columns={tableColumns}
+                        dataSource={resultList}
+                        // pagination={{
+                        //     position: ["bottomCenter"],
+                        // }}
+                        pagination={false}
+                        size={"middle"}
+                    />
+                </div>
                 <Row>
                     <Col span={11}>
                         <Divider orientation="left">Introduction</Divider>
@@ -95,11 +122,11 @@ export default function HomepageFeatures() {
                     </Col>
                     <Col span={2}></Col>
                     <Col span={11}>
-                        <Divider orientation="left">How to Contribute</Divider>
+                        <Divider orientation="left">Metrics</Divider>
                         <p>hehe</p>
                     </Col>
                 </Row>
-            </div>
+                </div>
         </section>
-    );
+);
 }
